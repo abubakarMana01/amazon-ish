0f0ab37489169f8c57e2442f4b987b2d7dc44aad
abubakarmana01 <ym.abubakr7@gmail.com>
2022-02-09 14:40:10 +0100

    Added empty orders page and changed header back button position

--- pages/orders.tsx
+++ pages/orders.tsx
@@ -1,11 +1,30 @@
-import { Header } from "components";
 import React from "react";
+import styles from "styles/Orders.module.css";
+import { useRouter } from "next/router";
+import { motion } from "framer-motion";
+import { Button, Header } from "components";
 
 export default function Orders() {
+	const router = useRouter();
+
 	return (
 		<main>
 			<Header />
-			orders
+
+			<motion.section
+				initial={{ opacity: 0, scale: 0.95 }}
+				animate={{ opacity: 1, scale: 1 }}
+				transition={{ type: "tween" }}
+				className={styles.orders__container}
+			>
+				<h1>Your Orders</h1>
+
+				<p>You&apos;ve not made any orders</p>
+				<Button
+					title="Continue Shopping"
+					handleClick={() => router.push("/")}
+				/>
+			</motion.section>
 		</main>
 	);
 }
