62d4621de65cf1d4b1204aa17c0091a5f42d8058
abubakarmana01 <ym.abubakr7@gmail.com>
2022-04-24 14:58:49 +0100

    Bookmarks page UI updated

--- pages/bookmarks.tsx
+++ pages/bookmarks.tsx
@@ -1,4 +1,4 @@
-import { Button, Header } from "components";
+import { Button, Header, Product } from "components";
 import React from "react";
 import styles from "styles/Bookmarks.module.css";
 import EmptyBookmarksImage from "assets/images/empty-bookmarks.svg";
@@ -6,7 +6,12 @@ import Image from "next/image";
 import { motion } from "framer-motion";
 import { useRouter } from "next/router";
 
-export default function Bookmarks() {
+interface IProps {
+	bookmarkedProducts: [];
+	error: any;
+}
+
+export default function Bookmarks({ bookmarkedProducts, error }: IProps) {
 	const router = useRouter();
 
 	return (
@@ -21,28 +26,59 @@ export default function Bookmarks() {
 			>
 				<h1>Bookmarks</h1>
 
-				<div className={styles.bookmarks__emptyContainer}>
-					<div className={styles.bookmarks__emptyImageWrapper}>
-						<div className={styles.bookmarks__emptyImageContainer}>
-							<Image
-								layout="fill"
-								src={EmptyBookmarksImage}
-								alt="empty-bookmarks"
+				{!bookmarkedProducts.length && !error ? (
+					<div className={styles.bookmarks__emptyContainer}>
+						<div className={styles.bookmarks__emptyImageWrapper}>
+							<div className={styles.bookmarks__emptyImageContainer}>
+								<Image
+									layout="fill"
+									src={EmptyBookmarksImage}
+									alt="empty-bookmarks"
+								/>
+							</div>
+						</div>
+
+						<div className={styles.bookmarks__emptyMessageContainer}>
+							<h2>It&apos;s empty here.</h2>
+							<p>
+								Something&apos;s catching your eye? Add your favorite items to
+								Bookmarks, and check them out anytime you wish.
+							</p>
+
+							<Button
+								title="Go Shopping"
+								handleClick={() => router.push("/")}
 							/>
 						</div>
 					</div>
-
-					<div className={styles.bookmarks__emptyMessageContainer}>
-						<h2>It&apos;s empty here.</h2>
-						<p>
-							Something&apos;s catching your eye? Add your favorite items to
-							Bookmarks, and check them out anytime you wish.
-						</p>
-
-						<Button title="Go Shopping" handleClick={() => router.push("/")} />
+				) : (
+					<div className={styles.bookmarks__productsContainer}>
+						{bookmarkedProducts.map((product: any) => (
+							<Product key={product.id} data={product} />
+						))}
 					</div>
-				</div>
+				)}
 			</motion.section>
 		</main>
 	);
+}
+
+export async function getStaticProps() {
+	let bookmarkedProducts = [];
+	let error = null;
+	try {
+		const res = await fetch("https://fakestoreapi.com/products");
+		bookmarkedProducts = await res.json();
+		bookmarkedProducts.length = 2;
+	} catch (err: any) {
+		console.log(err.message);
+		error = JSON.stringify(err);
+	}
+
+	return {
+		props: {
+			bookmarkedProducts,
+			error,
+		},
+	};
 }
