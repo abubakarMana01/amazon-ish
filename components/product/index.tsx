3c3c2fa0c1ebb707b31a64a74473818c324920e3
Abubakar Sadiq YAHYA <191212020@nunlab.com>
2022-04-27 12:15:27 +0100

    Added max-width to main - 2

--- components/product/index.tsx
+++ components/product/index.tsx
@@ -7,69 +7,69 @@ import Link from "next/link";
 import { motion } from "framer-motion";
 
 type productTypes = {
-	id: string;
-	image: string;
-	price: string;
-	title: string;
-	rating: { rate: number };
-	category: string;
+  id: string;
+  image: string;
+  price: string;
+  title: string;
+  rating: { rate: number };
+  category: string;
 };
 
 interface IProps {
-	data: productTypes;
+  data: productTypes;
 }
 
 export default function Product({ data }: IProps) {
-	return (
-		<motion.article
-			initial={{ opacity: 0, scale: 0.9 }}
-			animate={{ opacity: 1, scale: 1 }}
-			transition={{ type: "tween" }}
-			layout
-			className={styles.product__container}
-		>
-			<Link href={"/products/" + data.id}>
-				<a className={styles.product__link}>
-					<div className={styles.product__imageContainer}>
-						<Image
-							src={data.image}
-							width={230}
-							height={230}
-							objectFit="contain"
-							alt="product image"
-						/>
-					</div>
+  return (
+    <motion.article
+      initial={{ opacity: 0, scale: 0.9 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ type: "tween" }}
+      layout
+      className={styles.product__container}
+    >
+      <Link href={"/products/" + data.id}>
+        <a className={styles.product__link}>
+          <div className={styles.product__imageContainer}>
+            <Image
+              src={data.image}
+              width={230}
+              height={230}
+              objectFit="contain"
+              alt="product image"
+            />
+          </div>
 
-					<div className={styles.product__details}>
-						<div className={styles.product__category}>
-							<span>{data.category}</span>
-						</div>
+          <div className={styles.product__details}>
+            <div className={styles.product__category}>
+              <span>{data.category}</span>
+            </div>
 
-						<TextTruncate
-							line={3}
-							element="h2"
-							containerClassName={styles.product__title}
-							truncateText=""
-							text={data.title}
-						/>
+            <TextTruncate
+              line={3}
+              element="h2"
+              containerClassName={styles.product__title}
+              truncateText="..."
+              text={data.title}
+            />
 
-						<div className={styles.product__priceAndRatingContainer}>
-							<h3>{`$${data.price}`}</h3>
+            <div className={styles.product__priceAndRatingContainer}>
+              <h3>{`$${data.price}`}</h3>
 
-							<div className={styles.product__ratingContainer}>
-								<Star />
-								<span>{data.rating.rate}</span>
-							</div>
-						</div>
-					</div>
+              <div className={styles.product__ratingContainer}>
+                <Star />
+                <span>{data.rating.rate}</span>
+              </div>
+            </div>
+          </div>
 
-					{data.rating.rate < 3 && (
-						<div className={styles.product__banner}>
-							<span>Offer!</span>
-						</div>
-					)}
-				</a>
-			</Link>
-		</motion.article>
-	);
+          {data.rating.rate < 3 && (
+            <div className={styles.product__banner}>
+              <span>Offer!</span>
+            </div>
+          )}
+        </a>
+      </Link>
+    </motion.article>
+  );
 }
--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -3,9 +3,9 @@ import React from "react";
 import styles from "./styles.module.css";
 import { GetStaticPropsContext } from "next";
 import {
-	AddShoppingCartRounded,
-	BookmarkRounded,
-	LabelImportantRounded,
+  AddShoppingCartRounded,
+  BookmarkRounded,
+  LabelImportantRounded,
 } from "@mui/icons-material";
 import { Tooltip } from "@mui/material";
 import { Header, Product } from "components";
@@ -14,148 +14,148 @@ import { useAppContext } from "contexts";
 import { motion } from "framer-motion";
 
 export default function ProductDetail({
-	product,
-	suggestedProducts,
+  product,
+  suggestedProducts,
 }: {
-	product: {
-		id: string;
-		description: string;
-		image: string;
-		title: string;
-		price: string;
-		category: string;
-	};
-	suggestedProducts: [];
+  product: {
+    id: string;
+    description: string;
+    image: string;
+    title: string;
+    price: string;
+    category: string;
+  };
+  suggestedProducts: [];
 }) {
-	const appContext = useAppContext();
+  const appContext = useAppContext();
 
-	// Split the product description string to have an array
-	const description = product.description;
-	const splitDescription = description.split("; ");
+  // Split the product description string to have an array
+  const description = product.description;
+  const splitDescription = description.split("; ");
 
-	// Filter suggestions to have only products with the same category
-	let filteredSuggestions: any[] = [];
-	suggestedProducts.forEach((suggestedProduct: any) => {
-		if (
-			suggestedProduct.category === product.category &&
-			suggestedProduct.id !== product.id
-		) {
-			filteredSuggestions.push(suggestedProduct);
-		}
-	});
+  // Filter suggestions to have only products with the same category
+  let filteredSuggestions: any[] = [];
+  suggestedProducts.forEach((suggestedProduct: any) => {
+    if (
+      suggestedProduct.category === product.category &&
+      suggestedProduct.id !== product.id
+    ) {
+      filteredSuggestions.push(suggestedProduct);
+    }
+  });
 
-	return (
-		<main>
-			<Header />
+  return (
+    <main>
+      <Header />
 
-			<motion.section
-				initial={{ opacity: 0, y: -50 }}
-				animate={{ opacity: 1, y: 0 }}
-				className={styles.productDetail__container}
-			>
-				<div>
-					<div className={styles.productDetail__imageContainer}>
-						<img
-							src={product.image}
-							alt={"image of" + product.title}
-							style={{ objectFit: "contain", width: "100%", height: "100%" }}
-						/>
-					</div>
-				</div>
+      <motion.section
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        className={styles.productDetail__container}
+      >
+        <div>
+          <div className={styles.productDetail__imageContainer}>
+            <img
+              src={product.image}
+              alt={"image of" + product.title}
+              style={{ objectFit: "contain", width: "100%", height: "100%" }}
+            />
+          </div>
+        </div>
 
-				<div className={styles.productDetail__info}>
-					<h1>{product.title}</h1>
+        <div className={styles.productDetail__info}>
+          <h1>{product.title}</h1>
 
-					<ul className={styles.productDetails__description}>
-						{splitDescription.map((singleDesc, index) => (
-							<li key={index}>{singleDesc}</li>
-						))}
-					</ul>
+          <ul className={styles.productDetails__description}>
+            {splitDescription.map((singleDesc, index) => (
+              <li key={index}>{singleDesc}</li>
+            ))}
+          </ul>
 
-					<div className={styles.productDetail__actions}>
-						<h2 className={styles.productDetail__price}>
-							{"$" + product.price}
-						</h2>
-						<p className={styles.productDetail__deliveryMessage}>
-							<LabelImportantRounded />
-							Free Delivery Available - Abuja, Nigeria 900101
-						</p>
+          <div className={styles.productDetail__actions}>
+            <h2 className={styles.productDetail__price}>
+              {"$" + product.price}
+            </h2>
+            <p className={styles.productDetail__deliveryMessage}>
+              <LabelImportantRounded />
+              Free Delivery Available - Abuja, Nigeria 900101
+            </p>
 
-						<div className={styles.productDetail__buttons}>
-							<button className={styles.productDetail__cartButton}>
-								<AddShoppingCartRounded />
-								Add to Cart
-							</button>
+            <div className={styles.productDetail__buttons}>
+              <button className={styles.productDetail__cartButton}>
+                <AddShoppingCartRounded />
+                Add to Cart
+              </button>
 
-							<Tooltip placement="right" title="Bookmark" arrow>
-								<button className={styles.productDetail__bookmarkButton}>
-									<BookmarkRounded
-										style={{
-											fill: colors?.dark,
-											stroke: colors?.white,
-											strokeWidth: 2,
-											fontSize: 20,
-										}}
-									/>
-								</button>
-							</Tooltip>
-						</div>
-					</div>
-				</div>
-			</motion.section>
+              <Tooltip placement="right" title="Bookmark" arrow>
+                <button className={styles.productDetail__bookmarkButton}>
+                  <BookmarkRounded
+                    style={{
+                      fill: colors?.dark,
+                      stroke: colors?.white,
+                      strokeWidth: 2,
+                      fontSize: 20,
+                    }}
+                  />
+                </button>
+              </Tooltip>
+            </div>
+          </div>
+        </div>
+      </motion.section>
 
-			{filteredSuggestions.length && (
-				<section className={styles.productDetail__suggestionsContainer}>
-					<h2>You might also like</h2>
-					<div className={styles.product__suggestions}>
-						{filteredSuggestions?.slice(0, 3).map((suggestedProduct: any) => {
-							if (suggestedProduct.category === product.category) {
-								return (
-									<Product key={suggestedProduct.id} data={suggestedProduct} />
-								);
-							}
-							return null;
-						})}
-					</div>
-				</section>
-			)}
-		</main>
-	);
+      {filteredSuggestions.length && (
+        <section className={styles.productDetail__suggestionsContainer}>
+          <h2>You might also like</h2>
+          <div className={styles.product__suggestions}>
+            {filteredSuggestions?.slice(0, 3).map((suggestedProduct: any) => {
+              if (suggestedProduct.category === product.category) {
+                return (
+                  <Product key={suggestedProduct.id} data={suggestedProduct} />
+                );
+              }
+              return null;
+            })}
+          </div>
+        </section>
+      )}
+    </main>
+  );
 }
 
 export async function getStaticPaths() {
-	const res = await fetch("https://fakestoreapi.com/products");
-	const data = await res.json();
+  const res = await fetch("https://fakestoreapi.com/products");
+  const data = await res.json();
 
-	const paths = data.map((product: { id: string }) => ({
-		params: {
-			id: product.id.toString(),
-		},
-	}));
+  const paths = data.map((product: { id: string }) => ({
+    params: {
+      id: product.id.toString(),
+    },
+  }));
 
-	return {
-		paths,
-		fallback: false,
-	};
+  return {
+    paths,
+    fallback: false,
+  };
 }
 
 export async function getStaticProps(context: GetStaticPropsContext) {
-	const id = context.params?.id;
+  const id = context.params?.id;
 
-	// Fetch single product
-	const productResponse = await fetch(
-		"https://fakestoreapi.com/products/" + id
-	);
-	const product = await productResponse.json();
+  // Fetch single product
+  const productResponse = await fetch(
+    "https://fakestoreapi.com/products/" + id
+  );
+  const product = await productResponse.json();
 
-	// Fetch all products to be filtered
-	const productsResponse = await fetch("https://fakestoreapi.com/products");
-	const products = await productsResponse.json();
+  // Fetch all products to be filtered
+  const productsResponse = await fetch("https://fakestoreapi.com/products");
+  const products = await productsResponse.json();
 
-	return {
-		props: {
-			product,
-			suggestedProducts: products,
-		},
-	};
+  return {
+    props: {
+      product,
+      suggestedProducts: products,
+    },
+  };
 }
