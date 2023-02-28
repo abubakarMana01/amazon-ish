670ede47fdee14935f6a406d6b31c4a2a5243184
abubakarMana01 <ym.abubakr7@gmail.com>
2023-02-28 12:37:25 +0100

    fix: changed backend url

--- config/axios.ts
+++ config/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-	baseURL: "https://amazon-ish.herokuapp.com/",
+	baseURL: "https://amazon-ish-backend.onrender.com/",
 	timeout: 30000,
 });
 
